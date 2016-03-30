4b6cc671cc78c141eba5833fc8ca5aed54f1d3c8
BlinkingStar <steadies@naver.com>
2016-03-30 15:35:46 +0900

    160330

---
+++ OnlineJudge/ACM_ICPC_NET/2538.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 2*acos(0.0)
+#define INF 1e8
+#define EPSILON 1e-8
+#ifdef DEBUG
+#define DPRINTF(x) printf x
+#else
+#define DPRINTF(x) ;
+#endif
+
+int x[500001], y[500001], p[500001], n, w, h;
+
+inline pos(int ii) {
+	if (x[ii] == 0) return 0;
+	else if (y[ii] == 0) return 1;
+	else if (x[ii] == w) return 2;
+	else return 3;
+}
+
+inline bool isEdge (int ii) {
+	if (x[ii] == x[(ii+1)%n]
+int main () {
+
+	return 0;
+}
+
+
