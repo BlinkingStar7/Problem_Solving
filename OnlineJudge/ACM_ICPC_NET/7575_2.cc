1e68a268918f95b11142601920e2fbf4611dcf2b
blinkingstar7 <steadies@naver.com>
2016-04-21 23:13:55 +0900

    160421

--- OnlineJudge/ACM_ICPC_NET/7575_2.cc
+++ OnlineJudge/ACM_ICPC_NET/7575_2.cc
@@ -15,7 +15,7 @@ typedef vector<int> vi;
 typedef vector<pii> vpii;
 typedef vector<bool> vb;
 typedef vector<string> vs;
-
+int n, k, s[101], a[101][1001];
 int main () {
 	scanf("%d%d", &n, &k);
 	for (int i=0; i<n; ++i) {
@@ -24,14 +24,46 @@ int main () {
 			scanf("%d", &a[i][j]);
 	}
 
-	int m=0;
-	for (int i=0; i<s[0]; ++i) {
-	
-	for (int i=1; i<n; ++i) {
-		for (int start=0; start<size[i]-k; ++start) {
-			
+	for (int start=0; start+k<=s[0]; ++start) {
+		bool ans = true;
+		int f[1001] = {0};
+		int m = 0;
 
+		for (int i=start+1; i<s[0]; ++i) {
+			while (m>0 && a[0][i] != a[0][start+m]) m = f[start+m-1];
+			if (a[0][i] == a[0][start+m]) 
+				f[i] = ++m;
+		}
 
+		for (int i=1; i<n; ++i) {
+			int m = 0;
+			bool flag = false;
+			for (int j=0; j<s[i]; ++j) {
+				while (m>0 && a[i][j] != a[0][start+m]) m = f[start+m-1];
+				if (a[i][j] == a[0][start+m]) {
+					if (++m == k) { flag = true; break;}
+				}
+			}
+
+			if (flag) continue;
+
+			m = 0;
+			for (int j=s[i]-1; j>=0; --j) {
+				while (m>0 && a[i][j] != a[0][start+m]) m = f[start+m-1];
+				if (a[i][j] == a[0][start+m]) {
+					if (++m == k) { flag = true; break; }
+				}
+			}
+
+			if (!flag) { ans = false; break; }
+		}
+		if (ans) {
+			printf("YES\n");
+			return 0;
+		}
+	}
+
+	printf("NO\n");
 	return 0;
 }
 
